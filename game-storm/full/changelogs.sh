f5897e9578b9a4a09ef1a95a44208f9246caceb6
ihyakdg <82592132+ihyakdg@users.noreply.github.com>
2024-08-12 02:38:42 +0800

    Add files via upload

---
+++ game-storm/full/changelogs.sh
@@ -0,0 +1,6 @@
+echo "
+Game Storm | Full Version
+─V1.0 (10)
+ ├ Using Fast-Core for LAxeron
+ └ Use 45 Minutes Optimizing Cooldown
+"
---
+++ game-storm/full/help.sh
@@ -0,0 +1,7 @@
+echo "Game-Storm:
+Usage: ash gstorm [-p <package>|<options>]
+Options:
+  --help | -h : to print this
+  --info | -i : to check modules info
+  --changelogs | -cl : to check update log
+  --no-cooldown | -ncd : to skip cooldown"
