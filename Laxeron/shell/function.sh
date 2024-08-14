35384173f16f46d820e9334d9d3f08f8224690cd
ihyakdg <82592132+ihyakdg@users.noreply.github.com>
2024-08-14 23:29:31 +0800

    Add files via upload

---
+++ Laxeron/shell/function.sh
@@ -0,0 +1,192 @@
+export AXERON=true
+export EXPIRED=true
+export CORE="d8a97692ad1e71b1"
+local THISPATH="/sdcard/Android/data/com.fhrz.axeron/files"
+export FUNCTION="/data/local/tmp/axeron.function"
+export AXFUN=". $FUNCTION"
+export AXBIN="/data/local/tmp/axeron_bin"
+local whitelist_file="/sdcard/AxeronModules/.config/whitelist.list"
+#color
+local ORANGE='\033[38;2;255;85;3m'
+local GREY='\033[38;2;105;105;105m'
+local NC='\033[0m'
+#local
+local modulePath="/sdcard/AxeronModules"
+local cachePath="/sdcard/AxeronModules/.cache"
+local cash="/data/local/tmp/axeron_cash"
+local urlBin="https://raw.githubusercontent.com/ihyakdg/Laxeron/main/shell/bin"
+timeformat() { echo "$(date -d "@$1" +"%Y-%m-%d %H.%M.%S")"; }
+
+import() {
+	filename="$1"
+	file=$(find "$(dirname "$0")" -type f -name "$filename")
+	
+	if [ -z "$file" ]; then
+		    dir="$(dirname "$0")"
+		    while [ "$dir" != "/data/local/tmp/axeron_cash" ]; do
+			        # Cari file di direktori saat ini
+			        file=$(find "$dir" -maxdepth 1 -name "$filename")
+			        if [ -n "$file" ]; then
+			            file="$file"
+			            break
+			        fi
+			        dir="$(dirname "$dir")"
+		    done
+	fi
+	dos2unix $file
+	source $file
+ 	eval path_$(echo "$filename" | tr -cd '[:alnum:]_-')="$file"
+}
+
+#constructor
+axfun_construct() {
+	mkdir -p "$AXBIN"
+}
+
+rozaq() {
+	if [ -z "$1" ]; then
+		echo "Error: No text provided."
+		return 1
+	fi
+	
+	echo "r17$(echo -n "$1" | base64 | tr A-Za-z R-ZA-Qr-za-q)"
+}
+
+storm() {
+    	exec=false
+     	cacheExec=false
+    	file_name="response"
+     	runPath="$(dirname $0)"
+     	#echo "start $@"
+
+    	if [ $# -eq 0 ]; then
+        	echo "Usage: storm <URL> [options]"
+        	return 0
+    	fi
+
+	case $1 in
+	    --runPath|-rP) 
+	        if [ -d "$2" ]; then
+	        	runPath="$2"
+	        	shift 2
+	  	else
+    			shift 1
+       		fi
+	        ;;
+	esac
+ 	#echo "runPath $runPath"
+ 
+     	#local runPath="$(dirname $0)"
+    	local responsePath="${THISPATH}/response"
+    	local errorPath="${THISPATH}/error"
+
+    	case $1 in
+		--exec|-x) exec=true; api=$([[ "${2:0:3}" = "r17" ]] && echo "${2:3}" | tr R-ZA-Qr-za-q A-Za-z | base64 -d || echo "$2"); shift 2 ;;
+		* ) api=$1; shift ;;
+	esac
+
+	case $1 in
+		--fname|-fn) file_name="$2"; rm -f "{$runPath}/$file_name"; shift 2 ;;
+	esac
+ 	#echo "after case $@"
+
+    	if [ -z "$api" ]; then
+        	echo "Error: No API URL provided."
+        	return 1
+    	fi
+
+    	rm -f "$responsePath" "$errorPath"
+
+    	am startservice -n com.fhrz.axeron/.Storm --es api "$api" --es path "$responsePath" > /dev/null 2>&1
+
+	#testCacheExec
+ # 	if [ "$exec" = true ]; then
+	# 	if [ -f "${runPath}/$file_name" ]; then
+ #  			"${runPath}/$file_name" "$@"
+ #     			cacheExec=true
+ #     		fi
+	# fi
+
+    	while [ ! -e "$responsePath" ] && [ ! -e "$errorPath" ]; do
+        	#sleep 0.25
+    	done
+
+    	if [ -e "$responsePath" ]; then
+        	if [ "$exec" = true ]; then
+	 		#echo "storm -x $@"
+            		cp "$responsePath" "$runPath/$file_name"
+            		chmod +x "$runPath/$file_name"
+            		"${runPath}/$file_name" "$@"
+        	else
+            		echo -e $(cat "$responsePath")
+        	fi
+    	elif [ -e "$errorPath" ]; then
+        	echo -e "$(cat "$errorPath")"
+    	fi
+}
+
+toast() {
+	storm -rP "$AXBIN" -x "${urlBin}/toast.sh" -fn "toast" "$@"
+}
+
+openlink() {
+	am broadcast -a axeron.show.ADS --es url "$1" > /dev/null 2>&1
+}
+
+buyvip() {
+	openlink "https://sociabuzz.com/fahrezone/p/donatur-laxeron-vip-telegram-gc"
+}
+
+pkglist() {
+	storm -rP "$AXBIN" -x "${urlBin}/pkglist.sh" -fn "pkglist" "$@"
+}
+
+flaunch() {
+	if [ $# -eq 0 ]; then
+		echo "Usage: flaunch <package_name>"
+		return 0
+	fi
+	
+	am start --activity-no-animation -n $(cmd package dump "$1" | awk '/MAIN/{getline; print $2}' | head -n 1) > /dev/null 2>&1
+}
+
+deviceinfo() {
+device_info="
+Optione {
+	key:checkJit=\"$(getprop dalvik.vm.usejit)\";
+	key:checkVulkan=\"$(ls /system/lib/libvulkan.so > /dev/null 2>&1 && echo true || echo false)\";
+	key:infoRender=\"$(getprop debug.hwui.renderer)\";
+}
+"
+echo -e "$device_info"
+}
+
+xtorm() {
+	storm -x $@
+}
+
+whitelist() {
+	storm -rP "$AXBIN" -x "${urlBin}/whitelist.sh" -fn "whitelist" "$@"
+}
+
+jit() {
+	storm -rP "$AXBIN" -x "${urlBin}/jit.sh" -fn "jit" "$@"
+}
+
+axprop() {
+	storm -rP "$AXBIN" -x "${urlBin}/axprop.sh" -fn "axprop" "$@"
+}
+
+shx() {
+	storm -rP "$AXBIN" -x "${urlBin}/shx.sh" -fn "shx" "$@"
+}
+
+ax() {
+	storm -rP "$AXBIN" -x "${urlBin}/ax.sh" -fn "ax" "$@"
+}
+
+ax2() {
+	storm -rP "$AXBIN" -x "${urlBin}/ax2.sh" -fn "ax2" "$@"
+}
+
+axfun_construct $@
---
+++ Laxeron/shell/source.sh
@@ -0,0 +1,23 @@
+export AXERONPKG="!axPkg"
+export AXERONID="!axId"
+export AXERONVNAME="!axVName"
+export AXERONVCODE=!axVCode
+thisfunc="/data/local/tmp/axeron.function"
+thiscfg="/sdcard/AxeronModules/.prop"
+cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp
+chmod +x $thisfunc
+. $thisfunc
+if [ -f "$thiscfg" ]; then
+  dos2unix $thiscfg
+  . $thiscfg
+fi
+this_core=$(dumpsys package "$AXERONPKG" | grep "signatures" | cut -d '[' -f 2 | cut -d ']' -f 1)
+[[ -z $AXERONPKG || $AXERONPKG != "com.fhrz.axeron" ]] && echo "Something wrong, may be need Update?" && exit 0
+if ! echo "$CORE" | grep -q "$this_core"; then
+  echo "Axeron Not Original"
+  exit 0
+fi
+if storm "https://raw.githubusercontent.com/ihyakdg/Laxeron/main/ban_list.txt" | grep -iq "$AXERONID"; then
+  echo "You has ben banned"
+  exit 999
+fi
